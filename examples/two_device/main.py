0d992e742e6fdef2fa8616e4404d915e15796c98
W. Meijer <wouter.meijer@tno.nl>
2024-11-06 20:38:58 +0100

    feat: increase node limit to 5000, add clear rendered points functionality, and optimize point rendering

--- examples/two_device/main.py
+++ examples/two_device/main.py
@@ -5,15 +5,19 @@ from nicegui import app, run, ui
 
 import meshnetworkx as mx
 
-N_NODES = 500
+N_NODES = 5000
 M = mx.GraphZ()
 
+RENDERED_POINTS: dict[tuple[float, float], ui.scene.sphere] = {}
 
 nr_nodes = ui.label("nr_nodes")
 
 
 def _add_node(i=1):
-    M.add_node(hfid(), pos=(i, i))
+    NUMBER = 50
+    div = i // NUMBER
+    remaining = i % NUMBER
+    M.add_node(hfid(), pos=(div, remaining))
 
 
 def _add_n_nodes():
@@ -21,16 +25,47 @@ def _add_n_nodes():
         _add_node(i)
 
 
+def _clear_rendered_points():
+    RENDERED_POINTS.clear()
+    scene.clear()
+
+
 with ui.row().classes("w-full"):
-    ui.button("clear", on_click=M.clear)
+    ui.button("clear GraphZ", on_click=M.clear)
     ui.button("add Node", on_click=_add_node)
     ui.button(f"add {N_NODES} Nodes", on_click=_add_n_nodes)
+    ui.button("clear rendered points", on_click=_clear_rendered_points)
     ui.space()
     ui.button("shutdown", on_click=app.shutdown, color="negative")
 
 
 with ui.scene(height=700).classes("w-full") as scene:
     pass
+
+
+def _draw_pointcloud(nodes):
+    points = []
+    for _, data in nodes:
+        x, y = data["pos"]
+
+        points.append((x, y, 0))
+    colors = [(255, 0, 0)] * len(points)
+    scene.point_cloud(points, colors)
+
+
+def _draw(nodes):
+    read = set()
+    for _, data in nodes:
+        x, y = data["pos"]
+        read.add((x, y))
+
+    for x, y in read - RENDERED_POINTS.keys():
+        actor = scene.sphere().move(x, y, 0).material(color="red")
+        RENDERED_POINTS[(x, y)] = actor
+
+    for x, y in RENDERED_POINTS.keys() - read:
+        actor = RENDERED_POINTS.pop((x, y))
+        actor.delete()
 
 
 async def _read_graph():
@@ -43,14 +78,15 @@ async def _read_graph():
     nodes = await run.io_bound(wrap)
     nr_nodes.set_text(f"Got {len(nodes)} nodes")
     # info.set_text(str(nodes))
-    scene.clear()
+    # scene.clear()
 
-    for _, data in nodes:
-        x, y = data["pos"]
-        scene.sphere().move(x, y, 0).material(color="red")
+    await run.io_bound(_draw, nodes)
+
+    ui.notify("Graph updated")
 
 
-ui.timer(0.3, _read_graph)
+ui.timer(1, _read_graph)
+ui.timer(1, lambda: ui.notify("tick"))
 
 
 def _cleanup():
