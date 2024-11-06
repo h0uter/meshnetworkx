7f543a55cc513b894a7d9e75f139b3f6ddfa1edb
W. Meijer <wouter.meijer@tno.nl>
2024-11-06 19:59:43 +0100

    feat: refactor main.py to enhance node addition and improve UI layout

--- examples/two_device/main.py
+++ examples/two_device/main.py
@@ -1,50 +1,46 @@
 """This module demonstrates a simple NiceGUI application with a mesh network graph."""
 
-import random
-
 from humid import hfid
-from nicegui import app, run, ui
+from nicegui import app, ui
 
 import meshnetworkx as mx
 
-ui.label("Hi world")
-
 M = mx.GraphZ()
-
-ui.button("shutdown", on_click=app.shutdown)
-
-ui.button("clear", on_click=M.clear)
-
-
-def _add_node():
-    some_color = random.choice(["red", "blue", "green"])
-
-    M.add_node(hfid(), color=some_color)
-
-
-ui.button("add Node", on_click=_add_node)
-
-
-def _add_n_nodes():
-    N = 50000
-    for _ in range(N):
-        _add_node()
-
-
-ui.button("add N Nodes", on_click=_add_n_nodes)
-
 
 nr_nodes = ui.label("nr_nodes")
 info = ui.label("info")
 
 
-async def _read_graph():
-    def wrap():
-        return M.nodes(data=True)
+def _add_node(i=1):
+    M.add_node(hfid(), pos=(i, i))
 
-    nodes = await run.io_bound(wrap)
+
+def _add_n_nodes():
+    N = 50
+    for i in range(N):
+        _add_node(i)
+
+
+with ui.row():
+    ui.button("shutdown", on_click=app.shutdown, color="negative")
+    ui.button("clear", on_click=M.clear)
+    ui.button("add Node", on_click=_add_node)
+    ui.button("add N Nodes", on_click=_add_n_nodes)
+
+
+with ui.scene(height=700).classes("w-full") as scene:
+    pass
+
+
+async def _read_graph():
+    nodes = M.nodes(data=True)
     nr_nodes.set_text(f"Got {len(nodes)} nodes")
-    info.set_text(str(nodes))
+    # info.set_text(str(nodes))
+    scene.clear()
+
+    for _, data in nodes:
+        x, y = data["pos"]
+        scene.sphere().move(x, y, 0).material(color="red")
 
 
 ui.timer(0.3, _read_graph)
